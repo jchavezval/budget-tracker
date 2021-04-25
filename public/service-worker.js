f32fd3cb0865fe43a9815f7cbd32317d9132811a
Juan Chavez <jmchv2001@gmail.com>
2021-04-25 16:33:46 -0700

    finalized

---
+++ public/service-worker.js
@@ -0,0 +1,101 @@
+const { response } = require("express");
+
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/style.css",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icon-/icon-192x192.png",
+    "./icon-/icon-384x384.png",
+    "./icon-/icon-512x512.png",
+    "./js/idb.js",
+    "./js/index.js"
+];
+self.addEventListener('install', function(evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Your files were pre-cached succesfully!');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+})
+
+// Activate the service worker and remove old data from the cache
+// YOUR CODE HERE
+//
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    if (e.request.url.includes('/api')) {
+        e.respondWith(
+            caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+                return fetch(e.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(e.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(e.request);
+                });
+            })
+            .catch(err => console.log(err))
+        );
+        return;
+    }
+    e.respondWith(
+        fetch(e.request).catch(function() {
+            return caches.match(e.request).then(function(response) {
+                if (response) {
+                    return response;
+                } else if (e.request.headers.get('accept').includes('text/html')) {
+                    return caches.match('/');
+                }
+            });
+        })
+    );
+  })
+  
+  // Cache resources
+  self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+  })
+  
+  // Delete outdated caches
+  self.addEventListener('activate', function(e) {
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        // `keyList` contains all cache names under your username.github.io
+        // filter out ones that has this app prefix to create keeplist
+        let cacheKeeplist = keyList.filter(function(key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        // add current cache name to keeplist
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(
+          keyList.map(function(key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i]);
+              return caches.delete(keyList[i]);
+            }
+          })
+        );
+      })
+    );
+  });
