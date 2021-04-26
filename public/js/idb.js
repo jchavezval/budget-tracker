5bd944692c026963d75e7f0aa89e23c2ec98cb88
Juan Chavez <jmchv2001@gmail.com>
2021-04-25 17:22:25 -0700

    added in cors

--- public/js/idb.js
+++ public/js/idb.js
@@ -20,7 +20,7 @@ request.onsuccess = function (event) {
 
   // check if app is online, if yes run checkDatabase() function to send all local db data to api
   if (navigator.onLine) {
-    uploadTransaction();
+    // uploadTransaction();
   }
 };
 
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const compression = require("compression");
 
 const PORT = process.env.PORT || 3010;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-
+const cors = require("cors");
 const app = express();
 
 app.use(logger("dev"));
